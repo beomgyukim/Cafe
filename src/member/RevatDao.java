f587d80e1baab8b5be3cb02bf8b6d5d0f5182e2c
beomgyukim <66768716+beomgyukim@users.noreply.github.com>
2020-08-27 12:45:58 +0900

    8-27 수정본
    8-27 수정본

--- src/member/RevatDao.java
+++ src/member/RevatDao.java
@@ -126,5 +126,114 @@ public class RevatDao {
 		
 		return lists;
 	}
+	
+	public int deletelist(int rno) {
+		getConnection();
+		int cnt = -1;
+		String sql = "delete from rmem where rno=?";
+		
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, rno);
+			cnt = ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if(ps != null) {
+					ps.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			}catch(SQLException e ) {
+				
+			}
+		}
+		return cnt;
+	}
+	
+	public ArrayList<RevatBean> selectRevat(int rno){
+		getConnection();
+		ArrayList<RevatBean> lists = new ArrayList<RevatBean>();
+		String sql = "select * from rmem where rno=?";
+		
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, rno);
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				int rno1 = rs.getInt("rno");
+				String rname = rs.getString("rname");
+				String rnum = rs.getString("rnum");
+				String rdate = rs.getString("rdate");
+				String rtime = rs.getString("rtime");
+				
+				RevatBean rb = new RevatBean();
+				rb.setRno(rno1);
+				rb.setRname(rname);
+				rb.setRnum(rnum);
+				rb.setRdate(rdate);
+				rb.setRtime(rtime);
+				
+				
+				lists.add(rb);
+			}
+		} catch (SQLException e) {
+			System.out.println("getAllMebers");
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(ps != null) {
+					ps.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			}catch(SQLException e ) {
+				
+			}
+		}
+		
+		return lists;
+	}
+	
+	public int update(int rno,String rname,String rnum,String rdate,String rtime) {
+		getConnection();
+		int cnt = -1;
+		String sql = "update rmem set rname=? , rnum=? , rdate=? , rtime=? where rno=?";
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, rname);
+			ps.setString(2, rnum);
+			ps.setString(3, rdate);
+			ps.setString(4, rtime);
+			ps.setInt(5, rno);
+			
+			cnt = ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if(ps != null) {
+					ps.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			}catch(SQLException e ) {
+				
+			}
+		}
+		return cnt;
+	}
+
 }
 
